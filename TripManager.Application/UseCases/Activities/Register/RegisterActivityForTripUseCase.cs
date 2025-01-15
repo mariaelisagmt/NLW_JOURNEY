1f20557312e48d83dc5d8d35dba68b32951a72a4
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-01-14 23:05:35 -0300

    FEATURE: Add functions for activities

---
+++ TripManager.Application/UseCases/Activities/Register/RegisterActivityForTripUseCase.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TripManager.Communication.Requests;
+using TripManager.Communication.Responses;
+using TripManager.Exception;
+using TripManager.Exception.ExceptionBase;
+using TripManager.Infrastructure;
+using TripManager.Infrastructure.Entities;
+using FluentValidation.Results;
+using TripManager.Communication.Enums;
+
+namespace TripManager.Application.UseCases.Activities.Register;
+
+public class RegisterActivityForTripUseCase
+{
+    public ResponseActivityJson? Execute(Guid tripId, RequestRegisterActivityJson request)
+    {
+        var dbContext = new TripDbContext();
+        var trip = dbContext
+            .Trips
+            .Include(trip => trip.Activities)
+            .FirstOrDefault(trip => trip.Id == tripId);
+
+        Validate(trip, request);
+
+        var entity = new Activity
+        {
+            Name = request.Name,
+            Date = request.Date,
+            TripId = tripId,
+        };
+         
+        dbContext.Activities.Add(entity);
+        dbContext.SaveChanges();
+
+        return new ResponseActivityJson
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+            Date = entity.Date,
+            Status = (ActivityStatus)entity.Status,
+        };
+    }
+
+    private void Validate(Trip? trip, RequestRegisterActivityJson request)
+    {
+        if (trip is null)
+            throw new NotFoundException(ResourceErrorMessages.TRIP_NOT_FOUND);
+
+        var validator = new RegisterActivityValidator();
+        var result = validator.Validate(request);
+
+        if (!(request.Date >= trip.StartDate && request.Date <= trip.EndDate)) 
+        {
+            result.Errors.Add(new ValidationFailure("Date", ResourceErrorMessages.DATE_NOT_WITHIN_TRAVEL_PERIOD));
+        }
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
